d4e08f6460c95a1736c17be5f8fd394a79a66781
Dexnaw <arnaud-3@outlook.com>
2017-10-12 10:52:22 +0200

    commit HomeController + travis config

---
+++ src/main/java/be/heh/test_spring/HomeController.java
@@ -0,0 +1,18 @@
+package be.heh.test_spring;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HomeController {
+
+    @RequestMapping("7")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+}
--- src/main/java/be/heh/test_spring/TestSringApplication.java
+++ src/main/java/be/heh/test_spring/TestSringApplication.java
@@ -1,4 +1,4 @@
-package be.heh.test_sring;
+package be.heh.test_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
