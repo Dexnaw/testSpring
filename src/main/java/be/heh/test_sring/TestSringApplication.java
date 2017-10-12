432ecd53cff645c17f397496625f880b406f4ad6
Dexnaw <arnaud-3@outlook.com>
2017-10-12 10:46:16 +0200

    premier commit clean

---
+++ src/main/java/be/heh/test_sring/TestSringApplication.java
@@ -0,0 +1,12 @@
+package be.heh.test_sring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSringApplication.class, args);
+	}
+}
---
+++ src/test/java/be/heh/test_sring/TestSringApplicationTests.java
@@ -0,0 +1,16 @@
+package be.heh.test_sring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
